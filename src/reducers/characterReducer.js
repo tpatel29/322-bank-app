9d969b7719529b7fe60a5b9caae0ad19dc25ffa3
Bryan Nissen <bryannissen@gmail.com>
2022-04-06 19:32:42 -0400

    Add Default Character State

--- src/reducers/characterReducer.js
+++ src/reducers/characterReducer.js
@@ -1,6 +1,24 @@
+const generateID = () => {
+  return `${Date.now()}${Math.floor(Math.random() * 100)}`;
+};
 
+const DEFAULT_STATE = {
+  characters: [
+    { id: generateID(), name: 'Vlad', initiative: 8, type: 'player' },
+    { id: generateID(), name: 'Vesh', initiative: 5, type: 'player' },
+    { id: generateID(), name: 'Kritch', initiative: 22, type: 'player' },
+    { id: generateID(), name: 'Azreal', initiative: 15, type: 'player' },
+    { id: generateID(), name: 'Tasselhoff', initiative: 19, type: 'player' },
+    { id: generateID(), name: 'Fire Giant', initiative: 6, type: 'enemy' },
+    { id: generateID(), name: 'Frost Giant', initiative: 1, type: 'enemy' },
+    { id: generateID(), name: 'Storm Giant', initiative: 18, type: 'enemy' },
+    { id: generateID(),  name: 'Cloud Giant', initiative: 13, type: 'enemy' },
+  ],
+  players: [],
+  enemies: [],
+};
 
-const characterReducer = (state = {}, action) => {
+const characterReducer = (state = DEFAULT_STATE, action) => {
   return state;
 };
 
