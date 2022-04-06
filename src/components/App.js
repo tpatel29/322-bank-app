2458a55daf762bcc5841d4d5945736c222dc42af
Bryan Nissen <bryannissen@gmail.com>
2022-04-06 19:27:54 -0400

    Added App

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
 
+import TeamList from "./TeamList";
+import InitiativeList from "./InitiativeList";
+
 const App = () => {
   return (
-    <h1>Initiative Tracker</h1>
+    <div className="container">
+      <div className="row">
+
+        <div className="col-sm-4">
+          <TeamList />
+        </div>
+
+        <div className="col-sm-4">
+          <InitiativeList />
+        </div>
+
+        <div className="col-sm-4">
+          <TeamList />
+        </div>
+
+      </div>
+    </div>
   );
 };
 
