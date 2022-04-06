b15e6f5d6208cad88f7c8fc90e0762e3146a3cfd
Bryan Nissen <bryannissen@gmail.com>
2022-04-06 19:24:11 -0400

    Added List Stubs

---
+++ src/components/InitiativeList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const InitiativeList = (props) => {
+  return (
+    <h2>Initiative List</h2>
+  );
+}
+
+export default InitiativeList;
---
+++ src/components/TeamList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class TeamList extends React.Component {
+  render() {
+    return (
+      <h2>Team List</h2>
+    );
+  }
+}
+
+export default TeamList;
