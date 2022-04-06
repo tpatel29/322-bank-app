c1504581756225cee25fc0d5229bfdffd37003fb
Bryan Nissen <bryannissen@gmail.com>
2022-04-06 18:44:51 -0400

    Index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 
+import App from './compoents/App';
+
+ReactDOM.render(
+  <Provider>
+    <BrowserRouter>
+      <Route path="/" exact component={App} />
+    </BrowserRouter>
+  </Provider>,
+  document.querySelector('#root')
+);
