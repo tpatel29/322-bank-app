015f15e5b446c51d3e1c8a5bbc827920e4d18974
Bryan Nissen <bryannissen@gmail.com>
2022-04-13 19:55:33 -0400

    Team List

--- src/components/TeamList.js
+++ src/components/TeamList.js
@@ -4,10 +4,12 @@ import { connect } from 'react-redux';
 import TeamMember from './TeamMember';
 import AddPlayer from './AddPlayer';
 
+import { removeCharacter } from '../actions';
+
 class TeamList extends React.Component {
 
   onRemoveCharacter = (id) => {
-
+    this.props.removeCharacter(id);
   }
 
   render() {
@@ -43,4 +45,4 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps)(TeamList);
+export default connect(mapStateToProps, { removeCharacter })(TeamList);
