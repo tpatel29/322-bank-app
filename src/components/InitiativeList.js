d741d1ab265848f4d9cbea936effe5cb1efce9be
Bryan Nissen <bryannissen@gmail.com>
2022-04-13 19:57:48 -0400

    Add Turn controls

--- src/components/InitiativeList.js
+++ src/components/InitiativeList.js
@@ -21,7 +21,18 @@ const InitiativeList = (props) => {
 
   return (
     <div className="card">
-      <div>{/* Turn Controls */}</div>
+      <div className="d-flex justify-content-between align-items-center">
+        <button
+          type="button"
+          className="btn btn-primary">
+          Previous
+        </button>
+        <button
+          type="button"
+          className="btn btn-primary">
+          Next
+        </button>
+      </div>
 
       <ul className="list-group">
         { characterList }
