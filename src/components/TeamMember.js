032945bad80884b428b6f96afbdacea1301332ca
Bryan Nissen <bryannissen@gmail.com>
2022-04-13 19:43:36 -0400

    Team Member update

--- src/components/TeamMember.js
+++ src/components/TeamMember.js
@@ -5,6 +5,12 @@ const TeamMember = (props) => {
     <li className='list-group-item'
         key={props.index}>
       { props.character.name }
+      <button
+        type="button"
+        className="btn btn-danger float-right"
+        onClick={() => props.removeCharacter(props.character.id)}>
+        Delete
+      </button>
     </li>
   );
 };
