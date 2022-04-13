82541b6462beccacfda57933669a11ef37f2d3ea
Bryan Nissen <bryannissen@gmail.com>
2022-04-13 18:38:30 -0400

    Updated Init List

--- src/components/InitiativeList.js
+++ src/components/InitiativeList.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 const InitiativeList = (props) => {
+  console.log(props);
   return (
     <h2>Initiative List</h2>
   );
 }
 
-export default InitiativeList;
+function mapStateToProps(state) {
+  return {
+    characters: state.characters.characters,
+  };
+}
+
+export default connect(mapStateToProps)(InitiativeList);
