251965be07de2f68f0d6a15faa1bd0d6c78ab5be
Bryan Nissen <bryannissen@gmail.com>
2022-04-13 20:11:06 -0400

    Highlight current player

--- src/components/InitiativeItem.js
+++ src/components/InitiativeItem.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
 const InitiativeItem = (props) => {
-  const { name, initiative } = props;
+  const { name, initiative, isCurrentTurn } = props;
+
+  let classNames = 'list-group-item d-flex justify-content-between align-items-center';
+
+  if (isCurrentTurn) {
+    classNames = classNames.concat(' active');
+  }
+
   return (
-    <li className='list-group-item d-flex justify-content-between align-items-center'>
+    <li className={classNames}>
       { name }
       <span className="badge badge-primary badge-pull">
         { initiative }
