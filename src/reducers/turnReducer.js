687af316eff8d572e5793567c54d995184d8f452
Bryan Nissen <bryannissen@gmail.com>
2022-04-13 20:01:39 -0400

    Turn Reducer

--- src/reducers/turnReducer.js
+++ src/reducers/turnReducer.js
@@ -1,5 +1,21 @@
+const DEFAULT_STATE = {
+  current: 0,
+  numCharacters: 9,
+};
+
 const turnReducer = (state = {}, action) => {
-  return state;
+  switch (action.type) {
+    case 'NEXT_TURN':
+      state.current += 1;
+      return { ...state };
+
+    case 'PREV_TURN':
+      state.current -= 1;
+      return { ...state };
+
+    default:
+      return state;
+  }
 };
 
 export default turnReducer;
